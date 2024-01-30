418b53a9da933940ee25cc7568fd65fdb1057c6a
ayakhaled <aya.khaled@nweave.com>
2024-01-31 01:28:06 +0200

    -Added home menu btm_menu.xml. -Added fragment_home.xml for HomeFragment.kt screen. -Added fragment_favorites.xml for FavoritesFragment.kt screen. -Added fragment_category.xml  for CategoryFragment.kt screen. -Added fragment_detail.xml  for DetailFragment.kt screen. -Added ic_category.xml icon. -Added ic_favorite.xml icon. -Added ic_home.xml icon. -Compiled navController with BottomNavigationMenu in mainActivity

--- app/src/main/java/com/example/easyfood/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.easyfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btmNavigationView = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+//        val navController = Navigation.findNavController(this , R.id.nav_host_fragment_container)
+        NavigationUI.setupWithNavController(btmNavigationView , navController)
     }
 }
